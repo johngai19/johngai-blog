16225f40039681bccbeff27aa47de6c1505c8576
johngai19 <sandcabin83@gmail.com>
2026-04-08 17:47:41 +0800

    feat: redirect duplicate norwegian wood article slug

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/articles/walk-out-of-norwegian-wood',
+        destination: '/articles/%E8%B5%B0%E5%87%BA%E6%8C%AA%E5%A8%81%E7%9A%84%E6%A3%AE%E6%9E%97?lang=en',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default nextConfig
